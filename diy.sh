f14fc2cf51a104d09267848705b7287ed9037e07
bijiamo <61779399+bijiamo@users.noreply.github.com>
2020-03-05 03:07:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,4 +10,4 @@
 sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 rm package/kernel/mac80211/files/lib/wifi/mac80211.sh
-cp ../mac80211.sh package/kernel/mac80211/files/lib/wifi/
+cp ./mac80211.sh package/kernel/mac80211/files/lib/wifi/
